9a679235f407e22dec71204df7796623fbf9d721
Matheus Cruz <matheusd20m@gmail.com>
2022-09-02 13:58:15 -0300

    Crianção do projeto

---
+++ src/test/java/com/matheuscruz/vendas/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscruz.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
